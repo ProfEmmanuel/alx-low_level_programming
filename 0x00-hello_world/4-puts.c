869609eb5b025e387262d1496cc79f1040a629e3
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-13 17:45:36 -0700

    corrected 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-/**main - print "Programming is like building a multilingual puzzle
- * Return - 0
- */
+/*main - print "Programming is like building a multilingual puzzle*/
 
 int main(void)
 {
