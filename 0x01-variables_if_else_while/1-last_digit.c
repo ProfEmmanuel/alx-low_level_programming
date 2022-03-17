3e965071f36ba7d671d5d050a975183cd94a5fd8
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-17 08:33:28 -0700

    Corrected 1-last_digit.c to fit betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
 	printf("Last digit of %d is %d ", n, lastdigit);
-	if (lastdigit > 5){
+	if (lastdigit > 5)
+	{
 		printf("and is greater than 5");
 	}
-	else if (lastdigit == 0){
+	else if (lastdigit == 0)
+	{
 		printf("and is 0");
 	}
-	else if ((lastdigit < 6) & (lastdigit != 0)){
+	else if ((lastdigit < 6) & (lastdigit != 0))
+	{
 		printf("and is less than 6 and not 0");
 	}
 	printf("\n");
