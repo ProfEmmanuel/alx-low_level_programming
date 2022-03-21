e149704ba218a551d9ccce87212199e2b83d5691
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-21 14:53:28 -0700

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print digit 0 to 9 using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48, newline = 10;
+
+	do {
+		putchar(i);
+		i++;
+	} while
+	( i <= 57);
+	putchar(newline);
+	return (0);
+}
