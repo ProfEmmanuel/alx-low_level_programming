b678d5cec6b2e0d27c3f0fe02b2efdcf9061b370
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-21 15:28:02 -0700

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print digits 0 to 9 seperated by comma and space
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48,comma = 44, space = 32, newline = 10;
+
+	do {
+		putchar(i);
+		putchar(comma);
+		putchar(space);
+		i++;
+	} while
+	(i <= 57);
+	putchar(newline);
+	return (0);
+}
