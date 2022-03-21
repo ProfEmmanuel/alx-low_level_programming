317889d5c8ea454c36d5a66c1aa0440787c68578
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-21 05:16:13 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main: It print the lowercase alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	for(i; i <= 123; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
