4eae54c4592ee4d4d9522eea38962a470de36db6
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-28 02:54:53 -0700

    used _putchar function in 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main: Print the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i = "a";
+
+	while (i <= "z")
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
