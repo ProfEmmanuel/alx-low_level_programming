1455ed4c87d3965edf7a4b4814be67e75382a7dd
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-28 14:30:21 -0700

    Completed question 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  Compute the last digit of c
+ * @c: An integer
+ *
+ * Return: Returns the last digit
+ */
+int print_last_digit(int c)
+{
+	int last_digit;
+
+	if (c >= 0)
+	{
+		last_digit = c % 10;
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = c % 10;
+		return (-last_digit);
+	}
+	return (0);
+}
