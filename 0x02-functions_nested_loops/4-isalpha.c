a9f1bde82726893ed44617133948f7bfa0eae535
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-03-28 13:32:03 -0700

    Completed question 4 on alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a the argument is an alphabet
+ * @c: an integer
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a') & (c <= 'z'))
+	{
+		return (1);
+	}
+	else if ((c >= 'A') & (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
